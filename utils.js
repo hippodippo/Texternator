62be08e5b5feab2db4c9a648a068fb3cb00a4f8c
Kaycee Ingram <ingramkaycee@gmail.com>
2018-08-31 23:08:09 -0500

    refactor and use arrow functions

--- utils.js
+++ utils.js
@@ -4,7 +4,7 @@ const text = require('./text');
 
 module.exports = {
 
-  chop: function chop(date) {
+  chop: (date) => {
     let split_date = date.split('')
       , time = [];
 
@@ -17,7 +17,7 @@ module.exports = {
     return time.reverse().join('');
   },
 
-  fix_time:  function fix_time(time) {
+  fix_time: (time) => {
     let fixed_time = time.split(' ');
 
     fixed_time[fixed_time.length-1] = fixed_time[fixed_time.length-1].toUpperCase();
@@ -25,15 +25,15 @@ module.exports = {
     return fixed_time.join(' ');
   },
 
-  get_current_time:  function get_current_time() {
+  get_current_time: () => {
     return this.chop(dateFormat(new Date(), "dddd, mmmm dS, yyyy, h:MM TT"));
   },
 
-  check_time:  function check_time(time) {
+  check_time: (time) => {
     return this.get_current_time() === time ? true : false;
   },
 
-  fire:  function fire(message=null) {
+  fire: (message=null) => {
     if (message) text.smsBody = message;
 
     puretext.send(text, function (err, response) {
